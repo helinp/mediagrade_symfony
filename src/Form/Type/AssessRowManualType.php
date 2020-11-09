0c7b9d0e9eca670e9f247cdbc0cc4ab18657b538
helinp <pierre.helin@gmail.com>
2020-11-09 13:21:25 +0100

    add multi grading system

--- src/Form/Type/AssessRowManualType.php
+++ src/Form/Type/AssessRowManualType.php
@@ -3,35 +3,52 @@ namespace App\Form\Type;
 
 use App\Entity\Result;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AssessRowManualType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
+
+		$builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+			$result = $event->getData();
+			$form = $event->getForm();
+	
+			if($result->getAssessment()->getGradingSystem()->getId() === 1 )
+			{
+				$form->add('userLetter', ChoiceType::class, [
+					'choices'  => [
+						'Acquis' => 'A',
+						'En Aquisition' => 'EA',
+						'A Renforcer' => 'AR',
+						'Non Remis' => 'NR',
+						'Absent' => 'ABS',
+						'Non évalué' => 'NE'
+					]
+				]
+				);
+			}
+			else
+			{
+				$form->add('userVote', NumberType::class, [
+					'required' => false,
+					'attr' => ['size' => 3]
+				]);
+			}
+		});
+
+
 		$builder
 				->add('comment', TextType::class, [
 					'required' => false
 					])
-					->add('userVote', NumberType::class, [
-						'required' => false,
-						'attr' => ['size' => 3]
-				])
-					
-				/*->add('userLetter', ChoiceType::class, [
-			    'choices'  => [
-			        'Acquis' => 'A',
-			        'En Aquisition' => 'EA',
-			        'A Renforcer' => 'AR',
-			        'Non Remis' => 'NR',
-			        'Absent' => 'ABS',
-			        'Non évalué' => 'NE'
-			    ]
-				])*/
 				;
 	}
 
