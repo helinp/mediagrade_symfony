1bf73bee1d6c0bacb9b92be35b0bca7a0b49acd0
helinp <pierre.helin@gmail.com>
2021-02-27 16:18:11 +0100

    allow use of vote letters or numbers

--- src/Form/Type/AssessRowType.php
+++ src/Form/Type/AssessRowType.php
@@ -5,8 +5,10 @@ use App\Entity\Result;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 /**
@@ -16,20 +18,39 @@ class AssessRowType extends AbstractType
 {
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
+		$builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+			$result = $event->getData();
+			$form = $event->getForm();
+	
+			if($result->getAssessment()->getGradingSystem()->getId() === 1 )
+			{
+				$form->add('userLetter', ChoiceType::class, [
+					'required' => false,
+					'choices'  => [
+						'Acquis' => 'A',
+						'En Aquisition' => 'EA',
+						'A Renforcer' => 'AR',
+						'Non Remis' => 'NR',
+						'Absent' => 'ABS',
+						'Non évalué' => 'NE'
+					]
+				]
+				);
+			}
+			else
+			{
+				$form->add('userVote', NumberType::class, [
+					'required' => false,
+					'attr' => ['size' => 3]
+				]);
+			}
+		});
+
+
 		$builder
 				->add('comment', TextType::class, [
 					'required' => false
-				])
-				->add('userLetter', ChoiceType::class, [
-			    'choices'  => [
-			        'Acquis' => 'A',
-			        'En Aquisition' => 'EA',
-			        'A Renforcer' => 'AR',
-			        'Non Remis' => 'NR',
-			        'Absent' => 'ABS',
-			        'Non évalué' => 'NE'
-			    ]
-				])
+					])
 				;
 	}
 
